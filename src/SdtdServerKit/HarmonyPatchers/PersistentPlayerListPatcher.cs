766985ffe805eb99e87b4ca42735350238f65433
IceCoffee <1249993110@qq.com>
2024-08-24 12:18:00 +0800

    添加 Harmony 补丁以修改 PersistentPlayerList 行为
    在 `PersistentPlayerListPatcher.cs` 文件中进行了以下更改：
    
    1. 添加了 `using HarmonyLib;` 语句以引入 Harmony 库。
    2. 新增了一个命名空间 `SdtdServerKit.HarmonyPatchers`。
    3. 在该命名空间下，定义了一个内部类 `PersistentPlayerListPatcher`，并使用 `[HarmonyPatch(typeof(PersistentPlayerList))]` 特性对 `PersistentPlayerList` 类进行补丁。
    4. 在 `PersistentPlayerListPatcher` 类中，添加了一个前缀方法 `CleanupPlayers`，使用 `[HarmonyPrefix]` 和 `[HarmonyPatch(nameof(PersistentPlayerList.CleanupPlayers))]` 特性对 `PersistentPlayerList.CleanupPlayers` 方法进行补丁。
    5. 在 `CleanupPlayers` 方法中，将 `__result` 设置为 `false`，并返回 `false`，以阻止原始方法的执行。

---
+++ src/SdtdServerKit/HarmonyPatchers/PersistentPlayerListPatcher.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+
+namespace SdtdServerKit.HarmonyPatchers
+{
+    [HarmonyPatch(typeof(PersistentPlayerList))]
+    internal class PersistentPlayerListPatcher
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(PersistentPlayerList.CleanupPlayers))]
+        public static bool CleanupPlayers(ref bool __result)
+        {
+            __result = false;
+            return false;
+        }
+    }
+}
