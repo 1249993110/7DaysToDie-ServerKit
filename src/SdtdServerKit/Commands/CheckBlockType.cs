a1c49f3365c6be44086ccbc8a11ce08bfef172f2
IceCoffee <1249993110@qq.com>
2024-09-24 10:34:05 +0800

    Added CheckBlockType command class and related methods
    Added a reference to `SdtdServerKit.Managers`.
    
    Added a new `SdtdServerKit.Commands` namespace.
    
    Added a new `CheckBlockType` class in this namespace, which inherits from `ConsoleCmdBase`.
    
    The following methods are implemented in the `CheckBlockType` class:
    
    * `getDescription`: Returns the description of the command.
    
    * `getHelp`: Returns the help information of the command, including usage and parameter description.
    
    * `getCommands`: Returns an array of aliases for the command.
    
    * `Execute`: Executes the logic of the command, checks the type of the block at the specified position or under the player's feet according to the passed parameters, and outputs relevant information.
    
    In the `Execute` method, handles the situation where the number of parameters is incorrect, and records the error information when catching the exception.

---
+++ src/SdtdServerKit/Commands/CheckBlockType.cs
@@ -0,0 +1,80 @@
+﻿using SdtdServerKit.Managers;
+
+namespace SdtdServerKit.Commands
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class CheckBlockType : ConsoleCmdBase
+    {
+        /// <inheritdoc/>
+        public override string getDescription()
+        {
+            return "Checks the type of block by position or under your feet.";
+        }
+
+        /// <inheritdoc/>
+        public override string getHelp()
+        {
+            return "Usage:\n" +
+                "  1. bcheck {x} {y} {z}\n" +
+                "  2. bcheck\n" +
+                "1. check the block at x, y, z.\n" +
+                "2. check the block under your feet.";
+        }
+
+        /// <inheritdoc/>
+        public override string[] getCommands()
+        {
+            return new string[]
+            {
+                "ty-CheckBlockType",
+                "ty-cbt"
+            };
+        }
+
+        /// <inheritdoc/>
+        public override void Execute(List<string> args, CommandSenderInfo senderInfo)
+        {
+            try
+            {
+                if (args.Count != 0 && args.Count != 3)
+                {
+                    Log("ERR: Wrong number of arguments, expected 0 or 3, found " + args.Count.ToString() + ".");
+                    Log(this.GetHelp());
+                }
+                else
+                {
+                    int x, y ,z;
+                    if (args.Count == 0)
+                    {
+                        if (LivePlayerManager.TryGetByEntityId(senderInfo.GetEntityId(), out var managedPlayer) == false)
+                        {
+                            Log("ERR: Unable to get your position.");
+                            return;
+                        }
+
+                        var playerBlockPosition = managedPlayer!.EntityPlayer.GetBlockPosition();
+                        x = playerBlockPosition.x;
+                        y = playerBlockPosition.y - 1;
+                        z = playerBlockPosition.z;
+                    }
+                    else
+                    {
+                        int.TryParse(args[0], out x);
+                        int.TryParse(args[1], out y);
+                        int.TryParse(args[2], out z);
+                    }
+
+                    var position = new Vector3i(x, y, z);
+                    var block = GameManager.Instance.World.GetBlock(position);
+                    Log($"checking at {position}, block type: {block.type}, block name: {block.Block.GetBlockName()}.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Log("Error in CheckBlockType.Execute" + Environment.NewLine + ex.ToString());
+            }
+        }
+    }
+}
