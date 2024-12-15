85687cee0c004429b1201c3d09d14411cd275ad0
IceCoffee <1249993110@qq.com>
2024-12-15 13:37:56 +0800

    Add TaskScheduleSettings and TaskSchedule classes
    In `TaskScheduleSettings.cs`, import necessary namespaces, create `SdtdServerKit.FunctionSettings` namespace, and define `TaskScheduleSettings` class, inheriting from `SettingsBase`.
    
    In `TaskSchedule.cs`, import necessary namespaces, create `SdtdServerKit.Functions` namespace, and define `TaskSchedule` class, inheriting from `FunctionBase<TaskScheduleSettings>`. In the `TaskSchedule` class:
    - Define private fields `_cronDaemon`, `_taskScheduleRepository`, and `_commandListRepository`.
    - Define public property `CronDaemon`.
    - Define constructor to initialize fields.
    - Define the `OnEnableFunction` method to get and add the task schedule, start `_cronDaemon`.
    - Define the `OnDisableFunction` method to stop `_cronDaemon`.
    - Define the `AddTaskSchedule` method to create and add `CronJob`, execute the command list, and update the last running time of the task schedule.
    - Define the `RemoveTaskSchedule` method to remove the task schedule from `_cronDaemon`.
    - Define the `IsTaskScheduleRunning` method to check whether the task schedule is running.

---
+++ src/SdtdServerKit/Functions/TaskSchedule.cs
@@ -0,0 +1,81 @@
+﻿using IceCoffee.Cron;
+using SdtdServerKit.Data.Entities;
+using SdtdServerKit.Data.IRepositories;
+using SdtdServerKit.FunctionSettings;
+using System.Threading.Tasks;
+
+namespace SdtdServerKit.Functions
+{
+    public class TaskSchedule : FunctionBase<TaskScheduleSettings>
+    {
+        private readonly CronDaemon _cronDaemon = new CronDaemon();
+        private readonly ITaskScheduleRepository _taskScheduleRepository;
+        private readonly ICommandListRepository _commandListRepository;
+
+        /// <summary>
+        /// Cron daemon
+        /// </summary>
+        public CronDaemon CronDaemon => _cronDaemon;
+
+        public TaskSchedule(ITaskScheduleRepository taskScheduleRepository, ICommandListRepository commandListRepository)
+        {
+            _taskScheduleRepository = taskScheduleRepository;
+            _commandListRepository = commandListRepository;
+        }
+
+        protected override void OnEnableFunction()
+        {
+            var taskSchedules = _taskScheduleRepository.GetAll();
+
+            foreach (var item in taskSchedules)
+            {
+                if (item.IsEnabled)
+                {
+                    AddTaskSchedule(item);
+                }
+            }
+
+            _cronDaemon.Start();
+        }
+
+        protected override void OnDisableFunction()
+        {
+            _cronDaemon.Stop();
+        }
+
+        public void AddTaskSchedule(T_TaskSchedule taskSchedule)
+        {
+            var ronJob = new CronJob(taskSchedule.Id.ToString(), taskSchedule.CronExpression, async () =>
+            {
+                var commandList = await _commandListRepository.GetListByTaskScheduleIdAsync(taskSchedule.Id);
+                foreach (var item in commandList)
+                {
+                    foreach (var cmd in item.Command.Split('\n'))
+                    {
+                        Utilities.Utils.ExecuteConsoleCommand(cmd, item.InMainThread);
+                    }
+                    await Task.Delay(20);
+                }
+
+                taskSchedule.LastRunAt = DateTime.Now;
+                await _taskScheduleRepository.UpdateAsync(taskSchedule);
+            });
+            _cronDaemon.AddJob(ronJob);
+        }
+
+        public void RemoveTaskSchedule(int id)
+        {
+            _cronDaemon.RemoveJob(id.ToString());
+        }
+
+        public bool IsTaskScheduleRunning(int id)
+        {
+            if (_cronDaemon.CronJobs.TryGetValue(id.ToString(), out var job) && job.IsRunning)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
