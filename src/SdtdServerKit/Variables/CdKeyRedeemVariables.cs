1bcbdd0fd9c68455bc57879f1cd380715b9f26e2
IceCoffee <1249993110@qq.com>
2025-01-08 15:28:19 +0800

    Add new namespace and update property type
    In the `CdKeyRedeemVariables.cs` file, add a new namespace `SdtdServerKit.Variables`, and define an inner class `CdKeyRedeemVariables` in it. This class inherits from `VariablesBase` and adds a new nullable string property `CdKeyDescription` to describe the information of CdKey.
    
    In the `VipGiftVariables.cs` file, change the `GiftName` property from a normal string type to a `required` string type. This means that when instantiating the `VipGiftVariables` class, you must assign a value to the `GiftName` property.

---
+++ src/SdtdServerKit/Variables/CdKeyRedeemVariables.cs
@@ -0,0 +1,10 @@
+﻿namespace SdtdServerKit.Variables
+{
+    internal class CdKeyRedeemVariables : VariablesBase
+    {
+        /// <summary>
+        /// CdKey key description.
+        /// </summary>
+        public string? CdKeyDescription { get; set; }
+    }
+}
--- src/SdtdServerKit/Variables/VipGiftVariables.cs
+++ src/SdtdServerKit/Variables/VipGiftVariables.cs
@@ -2,7 +2,7 @@
 {
     public class VipGiftVariables : VariablesBase
     {
-        public string GiftName { get; set; }
+        public required string GiftName { get; set; }
 
         public int TotalClaimCount { get; set; }
 
